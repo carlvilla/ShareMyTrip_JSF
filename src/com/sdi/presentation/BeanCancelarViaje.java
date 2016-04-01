5b7ad59cae3bb7c992577a26a21d398827ec1d22
Sergio Moradas <sergimoradas@gmail.com>
2016-04-01 12:46:01 +0200

    Arreglados errores cancelar

--- src/com/sdi/presentation/BeanCancelarViaje.java
+++ src/com/sdi/presentation/BeanCancelarViaje.java
@@ -3,9 +3,12 @@ package com.sdi.presentation;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import com.sdi.business.TripsService;
 import com.sdi.infrastructure.Factories;
@@ -30,8 +33,16 @@ public class BeanCancelarViaje implements Serializable {
 		if(!viajesCancelar.isEmpty()){
 			TripsService trip = Factories.services.createTripService();
 			trip.cancelarViajes(viajesCancelar);
+			
+			FacesContext context = FacesContext.getCurrentInstance();
+			
+	        ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msgs");
+			
+	        context.addMessage(null, new FacesMessage(bundle.getString("Exito")
+	        		,bundle.getString("viajeCancelado")));
+	    	return "cancelTrip";
 		}
-		return "cancelTrip";
+		return "cancelFail";
 	}
 	
 
